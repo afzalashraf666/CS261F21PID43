e7046ff85160c9a9b65c2b66f4ea8484be2a57ab
Afzal Ashraf <78255759+afzalashraf666@users.noreply.github.com>
2021-10-15 21:01:24 +0500

    Create scraptest.py

---
+++ scraptest.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+
+genres = []
+
+driver = webdriver.Chrome(executable_path='C:\ProgramData\Anaconda3\chromedriver\chromedriver.exe')
+driver.get("https://www.chosic.com/list-of-music-genres/")
+content = driver.page_source
+soup = BeautifulSoup(content)
+
+for instance in soup.findAll('li' , attrs={'class':'capital-letter genre-term'}):
+    genre = instance.find('a', class_=False, id=False)
+    genres.append(genre.text)
+
+for genre in genres:
+    print(genre)
