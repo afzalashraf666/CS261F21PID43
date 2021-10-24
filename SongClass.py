de48c3c4db439bbb627d567709ef26ea6019aa5d
2020-cs-44 <92367651+2020-cs-44@users.noreply.github.com>
2021-10-24 15:49:12 +0500

    Add files via upload

---
+++ SongClass.py
@@ -0,0 +1,10 @@
+class Song:
+    def __init__(self, song_names, artist_names, plays, likes, comments, release_dates, reposts, genre_tags):
+        self.song_names = song_names
+        self.artist_names = artist_names
+        self.plays = plays
+        self.likes = likes
+        self.comments = comments
+        self.release_dates = release_dates
+        self.reposts = reposts
+        self.genre_tags = genre_tags
